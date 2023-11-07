d95d0dc8d4315986e7214dde12c6124d8ffe885c
cjl323 <99378157+cjl323@users.noreply.github.com>
2023-11-07 15:34:24 -0500

    Added some more files as well as the data sets that we will be using. Still need to clean up the data and add more to the skeleton

--- Data.py
+++ Data.py
@@ -1,1 +1,6 @@
-Data
+import pandas as pd
+
+#Read in data set
+fake_data = pd.read_csv('Fake.csv.zip')
+real_data = pd.read_csv('True.csv.zip')
+
--- Main.py
+++ Main.py
@@ -1,1 +1,5 @@
-Main
+#Train the AI on basic input
+#Read user input
+#parse the input
+#Put that input into the AI
+#Print out results
