ae571c7a4650e97251022e0a9c619d76687a3944
cjl323 <99378157+cjl323@users.noreply.github.com>
2023-11-07 15:47:22 -0500

    Cleaned up the files and started working on the main function

--- Data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-#Read in data set
-fake_data = pd.read_csv('Fake.csv.zip')
-real_data = pd.read_csv('True.csv.zip')
-
--- Main.py
+++ Main.py
@@ -3,3 +3,16 @@
 #parse the input
 #Put that input into the AI
 #Print out results
+import pandas as pd
+
+def main():
+    #Read in data set
+    #Data was taken from politifacts to determine if it was fake or real
+    #Apparently people found it easy to train the AI on the title
+    fake_data = pd.read_csv('data/Fake.csv.zip')
+    real_data = pd.read_csv('data/True.csv.zip')
+
+    print(fake_data)
+
+if __name__ == "__main__":
+    main()
